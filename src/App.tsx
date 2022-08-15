276fbae31b1e2c79c5872861a54362701827ff7b
Ariel Juodziukynas <arieljuod@gmail.com>
2022-08-14 21:35:15 -0300

    small import refactor

--- src/App.tsx
+++ src/App.tsx
@@ -12,8 +12,7 @@ import Settings from './screens/Settings'
 import Accessibility from './screens/Accessibility'
 import ContextProvider from './state/ContextProvider'
 import classNames from 'classnames'
-import { ControllerHints } from './components/UI'
-import OfflineMessage from './components/UI/OfflineMessage'
+import { ControllerHints, OfflineMessage } from './components/UI'
 
 function App() {
   const { epic, gog, contentFontFamily, actionsFontFamily, sidebarCollapsed } =
